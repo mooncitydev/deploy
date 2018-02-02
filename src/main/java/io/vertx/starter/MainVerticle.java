a8105ef1dd0e6423e9a0bb79d18a7976d450672e
Lewis Sall <lewissall@hotmail.com>
2018-02-02 13:29:25 -0500

    Deploy

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,14 +1,12 @@
 package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
-  public void start() {
-    vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hello Vert.x!"))
-        .listen(8080);
+  public void start(Future<Void> startFuture) throws Exception {
+	startFuture.complete();
   }
-
 }
